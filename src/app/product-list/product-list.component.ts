1e88e10db7e7c8d18cee360c6e07c57a993f07e5
fukuda <osaka481859@gmail.com>
2024-05-05 03:11:18 +0900

    product-listにrouterを追加

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [
+    NgFor,
+    RouterLink
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
